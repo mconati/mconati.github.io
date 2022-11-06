5be792803cf679747be78865fb7e8954b5298d0b
mconati <75708708+mconati@users.noreply.github.com>
2022-11-05 23:45:03 -0500

    test

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,20 +1,25 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $email_from = 'mcconati@gmail.com';
-    $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n"
-                        "User Message: $message.\n";
-    $to = "mcconati@gmail.com"
-
-    $headers = "From:  $email_from \r\n";
-
-    $headers .= "Reply To:  $visitor_email \r\n";
-
-    mail($to, $email_subject, $email_body, $headers);
-    header("Location: index.html");
-
-    ?>
+// Get data from form 
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+ 
+$to = "mcconati@gmail.com";
+$subject = "This is the subject line";
+ 
+// The following text will be sent
+// Name = user entered name
+// Email = user entered email
+// Message = user entered message
+$txt ="Name = ". $name . "\r\n  Email = "
+    . $email . "\r\n Message =" . $message;
+ 
+$headers = "From: noreply@demosite.com" . "\r\n" .
+            "CC: somebodyelse@example.com";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+ 
+// Redirect to
+header("Location:last.html");
+?>
